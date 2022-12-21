62f12da9f059727fa91922da1eb4aa2da1401c1b
legoloss3775 <timur3455@yandex.ru>
2022-12-22 01:55:48 +0500

    First commit

---
+++ FileManager.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using NLog;
+
+namespace Zalirun.Extentions
+{
+    public enum WriteMode
+    {
+        Overwrite,
+        Append,
+    }
+    public static class FileManager
+    {
+        private static readonly ILogger s_logger = NLog.LogManager.GetCurrentClassLogger();
+        private static readonly object s_locker = new object();
+
+        public static void WriteText(string path, string text, WriteMode mode)
+        {
+            lock (s_locker)
+            {
+                switch (mode)
+                {
+                    case WriteMode.Overwrite:
+                        try
+                        {
+                            System.IO.File.WriteAllText(path, text);
+                        }
+                        catch (Exception e)
+                        {
+                            s_logger.Error(e);
+                        }
+                        break;
+                    case WriteMode.Append:
+                        try
+                        {
+                            using (StreamWriter sw = System.IO.File.AppendText(path))
+                            {
+                                sw.WriteLine(text);
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            s_logger.Error(e);
+                        }
+                        break;
+                }
+            }
+        }
+
+        public static string ReadText(string path, Action<string> onLineReaded = null)
+        {
+            if (!System.IO.File.Exists(path))
+            {
+                return "";
+            }
+
+            lock (s_locker)
+            {
+                try
+                {
+                    string data = "";
+                    using (StreamReader sr = System.IO.File.OpenText(path))
+                    {
+                        string s = "";
+                        while ((s = sr.ReadLine()) != null)
+                        {
+                            data += s;
+                            onLineReaded?.Invoke(s);
+                        }
+                    }
+                    return data;
+                }
+                catch (Exception e)
+                {
+                    s_logger.Error(e);
+                }
+                return "";
+            }
+        }
+
+        public static void WriteJson<T>(string path, T data)
+        {
+            lock (s_locker)
+            {
+                try
+                {
+                    var json = JsonConvert.SerializeObject(data, Formatting.Indented);
+                    System.IO.File.WriteAllText(path, json);
+                }
+                catch (Exception e)
+                {
+                    s_logger.Error(e);
+                }
+            }
+        }
+
+        public static T ReadJson<T>(string path)
+        {
+            s_logger.Info($"Begin reading file : {path}");
+            if (!System.IO.File.Exists(path))
+            {
+                s_logger.Info($"File not found at path: {path}");
+                return default;
+            }
+
+            lock (s_locker)
+            {
+                using (FileStream openStream = System.IO.File.OpenRead(path))
+                {
+                    try
+                    {
+                        string data = "";
+                        using (StreamReader sr = System.IO.File.OpenText(path))
+                        {
+                            data += sr.ReadToEnd();
+                        }
+                        T obj = JsonConvert.DeserializeObject<T>(data);
+                        return obj;
+                    }
+                    catch (Exception e)
+                    {
+                        s_logger.Error(e);
+                        return default(T);
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ TimerManager.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Timers;
+using NLog;
+
+namespace Zalirun.Extentions
+{
+    public static class TimerManager
+    {
+        public static Dictionary<Guid, Timer> Timers { get; set; } = new Dictionary<Guid, Timer>();
+        private static readonly ILogger s_logger = NLog.LogManager.GetCurrentClassLogger();
+        public static Timer SetTimer(double interval, bool autoReset, out Guid timerId)
+        {
+            timerId = Guid.NewGuid();
+            if (interval < 0 || interval >= 2147483647)
+            {
+                var emptyTimer = new Timer();
+                s_logger.Error(new ArgumentException(), $"Timer interval argument exception, timer won't start : {interval}");
+                Timers.Add(timerId, emptyTimer);
+                return emptyTimer;
+            }
+            var timer = new Timer(interval)
+            {
+                AutoReset = autoReset,
+                Enabled = true
+            };
+            var removeTimerId = timerId;
+            timer.Elapsed += (sender, e) =>
+            {
+                if (!autoReset)
+                {
+                    ClearTimers(removeTimerId);
+                }
+            };
+            Timers.Add(timerId, timer);
+            s_logger.Info($"Created Timer Id - {timerId}, interval - {(DateTime.Now.AddMilliseconds(interval) - DateTime.Now)}");
+            return timer;
+        }
+        public static void ClearAllTimers()
+        {
+            foreach (var timer in Timers.ToList())
+            {
+                timer.Value.Stop();
+                timer.Value.Dispose();
+                Timers.Remove(timer.Key);
+
+            }
+        }
+        public static void ClearTimers(params Guid[] ids)
+        {
+            s_logger.Info($"Begin >> Clearing Timers");
+            foreach (var id in ids)
+            {
+                if (!Timers.ContainsKey(id))
+                {
+                    s_logger.Warn($"Timer not found with id {id}");
+                    continue;
+                }
+                var timer = Timers[id];
+                timer.Stop();
+                timer.Dispose();
+                Timers.Remove(id);
+                s_logger.Info($"Timer removed Id - {id}");
+            }
+            s_logger.Info($"End >> Clearing timers");
+        }
+        public static void ClearTimers(List<Guid> ids)
+        {
+            s_logger.Info($"Begin >> Clearing Timers");
+            foreach (var id in ids)
+            {
+                if (!Timers.ContainsKey(id))
+                {
+                    s_logger.Warn($"Timer not found with id {id}");
+                    continue;
+                }
+                var timer = Timers[id];
+                timer.Stop();
+                timer.Dispose();
+                Timers.Remove(id);
+                s_logger.Info($"Timer removed Id - {id}");
+            }
+            s_logger.Info($"End >> Clearing timers");
+        }
+    }
+}
